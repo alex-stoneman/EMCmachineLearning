016fda4f04c51995915a027ecfe0b4ddd13a0474
ImperialGold <alexhenrystoneman@gmail.com>
2022-07-05 00:30:20 +0100

    Initial commit

---
+++ LearningProjects/helloWorld.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+from six.moves import urllib
+
+# noinspection PyUnresolvedReferences
+import tensorflow.compat.v2.feature_column as fc
+
+import tensorflow as tf
+
+# Load dataset - label is survive: survive = 1, not survive = 0
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv') # training data
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') # testing data
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
+
+print(dftrain.head())
+print(y_train.head())
