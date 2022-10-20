73a8b08e742cb66513f14f6def89fe8d933f7ee3
ImperialGold <alexhenrystoneman@gmail.com>
2022-10-21 00:56:24 +0100

    Development of neural network

--- LearningProjects/ClothesRecognition/firstNeuralNetwork.py
+++ LearningProjects/ClothesRecognition/firstNeuralNetwork.py
@@ -5,7 +5,9 @@ from tensorflow import keras
 import numpy as np
 import os
 from matplotlib import pyplot as plt
-checkpoint_path = "training_1/cp.ckpt"
+number = int(input("Which model are you using: "))
+checkpoint_path = f"ClothesRecognition/training_{number}/cp.ckpt"
+
 checkpoint_dir = os.path.dirname(checkpoint_path)
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                    'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
@@ -20,11 +22,28 @@ fashion_mnist = tf.keras.datasets.fashion_mnist  # load dataset
 # plt.colorbar()
 # plt.grid(False)
 # plt.show()
-model = keras.Sequential([
+
+if number == 1: # 88.2%
+    model = keras.Sequential([
         keras.layers.Flatten(input_shape=(28, 28)),  # input layer (1)
         keras.layers.Dense(128, activation='relu'),  # hidden layer (2)
-        keras.layers.Dense(10, activation='softmax') # output layer (3)
+        keras.layers.Dense(10, activation='softmax')  # output layer (3)
     ])
+elif number == 2:# 88.8%
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),  # input layer (1)
+        keras.layers.Dense(128, activation='relu'),  # hidden layer (2)
+        keras.layers.Dense(64, activation='relu'),  # hidden layer (3)
+        keras.layers.Dense(10, activation='softmax')  # output layer (4)
+    ])
+else: # Model 3: 88.9%, Model 4: 89.5%
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),  # input layer (1)
+        keras.layers.Dense(128, activation='relu'),  # hidden layer (2)
+        keras.layers.Dense(128, activation='relu'),  # hidden layer (3)
+        keras.layers.Dense(10, activation='softmax')  # output layer (4)
+        ])
+
 model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
@@ -37,25 +56,24 @@ def train_a_neural_network():
                                                      save_weights_only=True,
                                                      verbose=1)
     # Trains the model for 10 epochs and saves the model after each epoch
-    model.fit(train_images, train_labels, epochs=10, callbacks=[cp_callback])
+    model.fit(train_images, train_labels, epochs=50, callbacks=[cp_callback])
     # Evaluate the model which has benn created
 
 
-def use_model():
+def use_model(display=False):
     model.load_weights(checkpoint_path)
     test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=1)
+    predictions = model.predict(test_images)
     print('Test accuracy:', test_acc)
     print("Test loss", test_loss)
-    # Predict the labels for the training set
-    predictions = model.predict(test_images)
-    print(predictions[0])
-    print(np.argmax(predictions[0]))
-    print(test_labels[0])
-    for x in range(len(test_images)):
-        plt.imshow(test_images[x])
-        plt.title(f"{class_names[np.argmax(predictions[x])]} = {class_names[test_labels[x]]}")
-        plt.show()
-        plt.close()
+    if display:
+        for x in range(len(test_images)):
+            plt.title(f"{class_names[np.argmax(predictions[x])]} = {class_names[test_labels[x]]}")
+            plt.imshow(test_images[x])
+            plt.show(block=False)
+            plt.figure()
+            time.sleep(0.2)
+            plt.close()
 
-# train_a_neural_network()
+#train_a_neural_network()
 use_model()
