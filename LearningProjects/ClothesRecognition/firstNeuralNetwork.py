7c5a35fd7ed52d31b03721053e12a9138d917528
ImperialGold <alexhenrystoneman@gmail.com>
2023-02-23 20:59:01 +0000

    hmmm

--- LearningProjects/ClothesRecognition/firstNeuralNetwork.py
+++ LearningProjects/ClothesRecognition/firstNeuralNetwork.py
@@ -1,11 +1,10 @@
-import time
-
+import time, os
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import os
 from matplotlib import pyplot as plt
-number = int(input("Which model are you using: "))
+
+number = int(input("Which model are you using: ")) # which file to save a model to / load a model from
 checkpoint_path = f"training_{number}/cp.ckpt"
 
 checkpoint_dir = os.path.dirname(checkpoint_path)
@@ -15,6 +14,9 @@ fashion_mnist = tf.keras.datasets.fashion_mnist  # load dataset
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()  # split into testing and training
 
 print(train_images.shape)
+print(train_images[0])
+print(train_images[0][0])
+print(train_images[0][0][0])
 # print(train_images[0, 23, 23])
 # print(train_labels)
 #
@@ -24,41 +26,34 @@ print(train_images.shape)
 # plt.grid(False)
 # plt.show()
 
-if number == 1: # 88.2%
+if number in [11, 12, 13, 14, 15]:
     model = keras.Sequential([
-        keras.layers.Flatten(input_shape=(28, 28)),  # input layer (1)
-        keras.layers.Dense(128, activation='relu'),  # hidden layer (2)
-        keras.layers.Dense(10, activation='softmax')  # output layer (3)
+        keras.layers.Flatten(input_shape=(28, 28)),  # input layer
+        keras.layers.Dense(128, activation='relu'),  # hidden layer
+        keras.layers.Dense(10, activation='softmax')  # output layer
     ])
-elif number == 2:# 88.8%
+elif number in [21, 22, 23, 24, 25]:
     model = keras.Sequential([
-        keras.layers.Flatten(input_shape=(28, 28)),  # input layer (1)
-        keras.layers.Dense(128, activation='relu'),  # hidden layer (2)
-        keras.layers.Dense(64, activation='relu'),  # hidden layer (3)
-        keras.layers.Dense(10, activation='softmax')  # output layer (4)
+        keras.layers.Flatten(input_shape=(28, 28)),  # input layer
+        keras.layers.Dense(128, activation='relu'),  # hidden layer
+        keras.layers.Dense(128, activation='relu'),  # hidden layer
+        keras.layers.Dense(10, activation='softmax')  # output layer
     ])
-elif number ==3 or number == 4: # Model 3: 88.9%, Model 4: 89.5%
+else:
     model = keras.Sequential([
-        keras.layers.Flatten(input_shape=(28, 28)),  # input layer (1)
-        keras.layers.Dense(128, activation='relu'),  # hidden layer (2)
-        keras.layers.Dense(128, activation='relu'),  # hidden layer (3)
-        keras.layers.Dense(10, activation='softmax')  # output layer (4)
+        keras.layers.Flatten(input_shape=(28, 28)),  # input layer
+        keras.layers.Dense(128, activation='relu'),  # hidden layer
+        keras.layers.Dense(128, activation='relu'),  # hidden layer
+        keras.layers.Dense(128, activation='relu'),  # hidden layer
+        keras.layers.Dense(10, activation='softmax')  # output layer
         ])
-else: # Model 5: 88.4%
-    model = keras.Sequential([
-        keras.layers.Flatten(input_shape=(28, 28)),  # input layer (1)
-        keras.layers.Dense(128, activation='relu'),  # hidden layer (2)
-        keras.layers.Dense(64, activation='relu'),  # hidden layer (3)
-        keras.layers.Dense(32, activation='relu'),  # hidden layer (3)
-        keras.layers.Dense(16, activation='relu'),  # hidden layer (3)
-        keras.layers.Dense(10, activation='softmax')  # output layer (4)
-    ])
 
 
 model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
-train_images = train_images / 255.0
+
+train_images = train_images / 255.0 # Transform values to the range of 0 - 1
 test_images = test_images / 255.0
 
 def train_a_neural_network():
@@ -69,7 +64,7 @@ def train_a_neural_network():
                                                      verbose=1)
     # Trains the model for 10 epochs and saves the model after each epoch
     model.fit(train_images, train_labels, epochs=numOfEpoch, callbacks=[cp_callback])
-    # Evaluate the model which has benn created
+    # Evaluate the model which has been created
 
 
 def use_model(display=False):
@@ -78,14 +73,17 @@ def use_model(display=False):
     predictions = model.predict(test_images)
     print('Test accuracy:', test_acc)
     print("Test loss", test_loss)
-    if display:
+    input("Press enter to begin test")
+    if display: # Display the images with the correct label and the prediction made
         for x in range(len(test_images)):
             plt.title(f"{class_names[np.argmax(predictions[x])]} = {class_names[test_labels[x]]}")
-            plt.imshow(test_images[x])
+            plt.imshow(test_images[x], cmap=plt.cm.binary)
+            plt.axis("off")
             plt.show(block=False)
             plt.figure()
             time.sleep(0.2)
             plt.close()
+
 
 # train_a_neural_network()
 use_model(True)
